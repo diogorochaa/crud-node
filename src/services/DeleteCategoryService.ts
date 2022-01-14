ee95053b636a575989f056a9f0a82557e6975376
DiogoRocha10 <diogorocha12@live.com>
2022-01-14 11:54:59 -0300

    feat(services/routes)finalizando crud completo e criando os videos

---
+++ src/services/DeleteCategoryService.ts
@@ -0,0 +1,14 @@
+import { Category } from "./../entities/Category";
+import { getRepository } from "typeorm";
+
+export class DeleteCategoryService {
+  async execute(id: string) {
+    const repo = getRepository(Category);
+
+    if (!(await repo.findOne(id))) {
+      return new Error("Category does not exists!");
+    }
+    await repo.delete(id);
+    return "Category exclude";
+  }
+}
